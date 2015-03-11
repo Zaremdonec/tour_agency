0d96cb4dda1e14d35fe5d1e6a432a010ee756020
Tenfrow <vit2zremb@gmail.com>
2015-03-11 18:03:25 +0200

    Added connection to database

--- classes/Database.php
+++ classes/Database.php
@@ -10,6 +10,11 @@ namespace classes;
 
 class Database {
     protected static $_instance;
+    private $_host = "localhost";
+    private $_username = "root";
+    private $_password = "";
+    private $_database = "blog_db";
+    private $_connection;
 
     static public function getInstance()
     {
@@ -21,9 +26,19 @@ class Database {
 
     private function __construct()
     {
-
+        $this->_connection = new \mysqli($this->_host, $this->_username,
+            $this->_password, $this->_database) or die("Database connection error");
     }
 
     private function __clone() { }
 
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+
+    public function query($command)
+    {
+        return $this->getConnection()->query($command);
+    }
 }
