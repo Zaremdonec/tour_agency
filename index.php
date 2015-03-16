81eb6756fbf60a68f1c01211b53808e066f420c1
Tenfrow <vit2zremb@gmail.com>
2015-03-16 10:58:05 +0200

    Revert "Single item"

--- index.php
+++ index.php
@@ -13,86 +13,86 @@
 		<div class="content">
 			<div class="tour">
 				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
+					<img src="../images/backdround.jpg">
 				</div>
 				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру ,asjfbmabsjdhbasdf ksfdkj asdf sakdjfg sadfasdf ghasdfas kdfasdfsadf h alskdf</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
+					<a href="#"><h2>Заголовок</h2></a>
 				</div>
 				<div class="text">
 					<p>Опис туру</p>
 				</div>
 				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
+					<a href="#">Детальніше</a>
 				</div>
 			</div>
 			<div class="tour">
 				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
+					<img src="../images/backdround.jpg">
 				</div>
 				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
+					<a href="#"><h2>Заголовок</h2></a>
 				</div>
 				<div class="text">
 					<p>Опис туру</p>
 				</div>
 				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
+					<a href="#">Детальніше</a>
 				</div>
 			</div>
 			<div class="tour">
 				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
+					<img src="../images/backdround.jpg">
 				</div>
 				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
+					<a href="#"><h2>Заголовок</h2></a>
 				</div>
 				<div class="text">
 					<p>Опис туру</p>
 				</div>
 				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
+					<a href="#">Детальніше</a>
 				</div>
 			</div>
 			<div class="tour">
 				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
+					<img src="../images/backdround.jpg">
 				</div>
 				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
+					<a href="#"><h2>Заголовок</h2></a>
 				</div>
 				<div class="text">
 					<p>Опис туру</p>
 				</div>
 				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
+					<a href="#">Детальніше</a>
 				</div>
 			</div>
 			<div class="tour">
 				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
+					<img src="../images/backdround.jpg">
 				</div>
 				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
+					<a href="#"><h2>Заголовок</h2></a>
 				</div>
 				<div class="text">
 					<p>Опис туру</p>
 				</div>
 				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
+					<a href="#">Детальніше</a>
+				</div>
+			</div>
+			<div class="tour">
+				<div class="image">
+					<img src="../images/backdround.jpg">
+				</div>
+				<div class="title">
+					<a href="#"><h2>Заголовок</h2></a>
+				</div>
+				<div class="text">
+					<p>Опис туру</p>
+				</div>
+				<div class="information">
+					<a href="#">Детальніше</a>
 				</div>
 			</div>
 		</div>
