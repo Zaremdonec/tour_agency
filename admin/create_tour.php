914fa758305d0f0340f8179000743804bd56e9b6
Andriy5852 <andriy5852@gmail.com>
2015-03-17 14:17:01 +0200

    Видалити кнопку для відображення картинки

--- admin/create_tour.php
+++ admin/create_tour.php
@@ -17,7 +17,6 @@
 				<p>Завантаження картинки</p>
 				<form method="post" enctype="multipart/form-data">
 	        		<input type="file" name="fileToUpload" id="fileToUpload"/>
-	        		<input type="submit" name="loadImage" value="Upload Image"/>
 	        		<br><br>
 					<input type="text" name="title" placeholder="Заголовок статі">
 					<select name="category" plaseholder="Місто">
@@ -34,18 +33,7 @@
 					<input type="submit" name="createPost" value="Опублікувати статю"/><br>
 					<a href = "admin.php">Назад</a>
     			</form>
-			    <?php
-			    	if(isset($_POST['loadImage'])){
-			    	echo $db->showImage($_FILES["fileToUpload"]["name"]);
-			    	$_SESSION['image'] = $_FILES["fileToUpload"]["name"];
-			    	} 
-			    	if(isset($_POST['createPost'])){
-			    		if(!empty($_POST['title']) && !empty($_POST['editor']) && !empty($_SESSION['image']))
-			    			$db->addPost($_POST['title'],$_POST['editor'],$_SESSION['image']);
-			    		else echo "Заповніть всі поля";
-			    		}
-			    	 
-			    ?>
+			    
 			</div>
 	</div>
 </body>
