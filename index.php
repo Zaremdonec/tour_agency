9c18eb274becc21d8388504f412c43ed9191eb99
Illequidormit <pasha9657@gmail.com>
2015-03-18 10:33:41 +0200

    Print list of all tours

--- index.php
+++ index.php
@@ -21,90 +21,9 @@
 		</ul>
 	</div>
 		<div class="content">
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру ,asjfbmabsjdhbasdf ksfdkj asdf sakdjfg sadfasdf ghasdfas kdfasdfsadf h alskdf</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
-			<div class="tour">
-				<div class="image">
-					<a href="templates/tour_item.php"><img src="../images/backdround.jpg"></a>
-				</div>
-				<div class="title">
-					<a href="templates/tour_item.php"><h2>Заголовок</h2></a>
-				</div>
-				<div class="text">
-					<p>Опис туру</p>
-				</div>
-				<div class="information">
-					<a href="templates/tour_item.php">Детальніше</a>
-				</div>
-			</div>
+		<?php
+			$excursion->print_all_excursion();
+		?>
 		</div>
 	</div>
 </body>
