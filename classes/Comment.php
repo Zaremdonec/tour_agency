a46393540466fe5493b7b2156f2934d255332777
Tenfrow <vit2zremb@gmail.com>
2015-03-21 02:42:09 +0200

    Add method for creation and attaching comment to tour

--- classes/Comment.php
+++ classes/Comment.php
@@ -7,5 +7,21 @@
  */
 require_once 'Database.php';
 class Comment {
+    private $author;
+    private $text;
+    private $time;
 
+    public function __construct($author, $text, $time = null)
+    {
+        $this->author = $author;
+        $this->text = $text;
+        $this->time = ($time === null) ? time() : $time;
+    }
+
+    public function attach($tour_id)
+    {
+        $command = "INSERT INTO comments (author, text, datetime, tour_id)
+            VALUES ({$this->author}, {$this->text}, {$this->time}, $$tour_id)";
+        Database::getInstance()->query($command);
+    }
 }
